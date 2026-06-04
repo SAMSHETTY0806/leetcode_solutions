97104ea69a084db7ff27c05006c8a5214c8500ed
SAMPANNA <162988969+SAMSHETTY0806@users.noreply.github.com>
2026-06-04 08:51:13 +0000

    Sync LeetCode submission Runtime - 13 ms (82.37%), Memory - 19.6 MB (72.10%)

---
+++ solutions/0383-ransom-note/solution.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        str1,str2= Counter(ransomNote),Counter(magazine)
+        if str1&str2==str1:
+            return True
+        return False
+
