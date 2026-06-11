3b5a1e1a103e1835562ab11b56a619845ec24733
SAMPANNA <162988969+SAMSHETTY0806@users.noreply.github.com>
2026-06-11 09:25:55 +0000

    Sync LeetCode submission Runtime - 285 ms (66.13%), Memory - 96.5 MB (61.92%)

---
+++ solutions/2121-find-if-path-exists-in-graph/solution.py
@@ -0,0 +1,35 @@
+from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def validPath(
+        self,
+        n: int,
+        edges: List[List[int]],
+        source: int,
+        destination: int
+    ) -> bool:
+
+        graph = defaultdict(list)
+        seen = set()
+
+        # Build adjacency list
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        q = deque([source])
+        seen.add(source)
+
+        while q:
+            node = q.popleft()
+
+            if node == destination:
+                return True
+
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    q.append(nei)
+
+        return False
